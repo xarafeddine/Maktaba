0acac13ef4cc6bead9bc9fc599f1d2c1359fd1f1
xarafeddine <mohamed.ce.ao@gmail.com>
2024-10-08 23:03:39 +0100

    bad request response func

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -21,14 +21,13 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 		Genres  []string `json:"genres"`
 	}
 
-	// Use the new readJSON() helper to decode the request body into the input struct.
-	// If this returns an error we send the client the error message along with a 400
-	// Bad Request status code, just like before.
 	err := app.readJSON(w, r, &input)
 	if err != nil {
-		app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+		// Use the new badRequestResponse() helper.
+		app.badRequestResponse(w, r, err)
 		return
 	}
+
 	fmt.Fprintf(w, "%+v\n", input)
 }
 
