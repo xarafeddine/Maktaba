7616b3cb8c3fc1e695b46ca0726ea1057dd564ad
xarafeddine <mohamed.ce.ao@gmail.com>
2024-10-10 15:21:58 +0100

    input validation

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -2,29 +2,41 @@ package main
 
 import (
 	"fmt"
-	"time"
 
 	"net/http"
+	"time"
 
 	"myMovieApi.xaraf.net/internal/data"
+	"myMovieApi.xaraf.net/internal/validator"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
-	// Declare an anonymous struct to hold the information that we expect to be in the
-	// HTTP request body (note that the field names and types in the struct are a subset
-	// of the Movie struct that we created earlier). This struct will be our *target
-	// decode destination*.
 	var input struct {
-		Title   string   `json:"title"`
-		Year    int32    `json:"year"`
-		Runtime int32    `json:"runtime"`
-		Genres  []string `json:"genres"`
+		Title   string       `json:"title"`
+		Year    int32        `json:"year"`
+		Runtime data.Runtime `json:"runtime"`
+		Genres  []string     `json:"genres"`
 	}
-
 	err := app.readJSON(w, r, &input)
 	if err != nil {
-		// Use the new badRequestResponse() helper.
 		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	// Copy the values from the input struct to a new Movie struct.
+	movie := &data.Movie{
+		Title:   input.Title,
+		Year:    input.Year,
+		Runtime: input.Runtime,
+		Genres:  input.Genres,
+	}
+
+	// Initialize a new Validator.
+	v := validator.New()
+	// Call the ValidateMovie() function and return a response containing the errors if
+	// any of the checks fail.
+	if data.ValidateMovie(v, movie); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
 		return
 	}
 
--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -1,17 +1,38 @@
 package data
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
 
-// Declare a custom Runtime type, which has the underlying type int32 (the same as our
-// Movie struct field).
+var ErrInvalidRuntimeFormat = errors.New("invalid runtime format")
+
 type Runtime int32
 
-// Implement a MarshalJSON() method on the Runtime type so that it satisfies the
-// json.Marshaler interface. This should return the JSON-encoded value for the movie
-// runtime (in our case, it will return a string in the format "<runtime> mins").
+func (r *Runtime) UnmarshalJSON(jsonValue []byte) error {
+
+	unquotedJSONValue, err := strconv.Unquote(string(jsonValue))
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	parts := strings.Split(unquotedJSONValue, " ")
+
+	if len(parts) != 2 || parts[1] != "mins" {
+		return ErrInvalidRuntimeFormat
+	}
+
+	i, err := strconv.ParseInt(parts[0], 10, 32)
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	*r = Runtime(i)
+	return nil
+}
+
 func (r Runtime) MarshalJSON() ([]byte, error) {
 	// Generate a string containing the movie runtime in the required format.
 	jsonValue := fmt.Sprintf("%d mins", r)
