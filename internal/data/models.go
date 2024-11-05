d66e2052a78c17fca4f80c10716028637989f7cb
xarafeddine <mohamed.ce.ao@gmail.com>
2024-11-05 22:03:28 +0100

    user authentication

--- internal/data/models.go
+++ internal/data/models.go
@@ -10,16 +10,14 @@ var (
 	ErrEditConflict   = errors.New("edit conflict")
 )
 
-// Create a Models struct which wraps the MovieModel. We'll add other models to this,
-// like a UserModel and PermissionModel, as our build progresses.
 type Models struct {
 	Movies MovieModel
+	Users  UserModel // Add a new Users field.
 }
 
-// For ease of use, we also add a New() method which returns a Models struct containing
-// the initialized MovieModel.
 func NewModels(db *sql.DB) Models {
 	return Models{
 		Movies: MovieModel{DB: db},
+		Users:  UserModel{DB: db}, // Initialize a new UserModel instance.
 	}
 }
